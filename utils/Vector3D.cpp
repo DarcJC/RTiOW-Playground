5985d28f6f36dd6dd5f9d1485ee8a95e413137c1
DarcJC <me@darc.pro>
2021-12-17 01:23:59 +0800

    feat. finished vector3d class

--- utils/Vector3D.cpp
+++ utils/Vector3D.cpp
@@ -2,4 +2,49 @@
 // Created by i on 12/17/2021.
 //
 
-#include "Vector3F.h"
+#include "Vector3D.h"
+#include "math.h"
+
+Vector3D::Vector3D(): x(0), y(0), z(0) {}
+
+Vector3D::Vector3D(double x, double y, double z): x(x), y(y), z(z) {}
+
+Vector3D Vector3D::operator-() const { return {-x, -y, -z}; }
+
+double Vector3D::operator[](int i) const {
+    if (i == 1) return x;
+    if (i == 2) return y;
+    return z;
+}
+
+double &Vector3D::operator[](int i) {
+    if (i == 1) return x;
+    if (i == 2) return y;
+    return z;
+}
+
+Vector3D &Vector3D::operator+=(const Vector3D &v) {
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    return *this;
+}
+
+Vector3D &Vector3D::operator*=(const double t) {
+    x *= t;
+    y *= t;
+    z *= t;
+    return *this;
+}
+
+Vector3D &Vector3D::operator/=(const double t) {
+    return *this *= 1/t;
+}
+
+double Vector3D::length() const {
+    return std::sqrt(length_squared());
+}
+
+double Vector3D::length_squared() const {
+    return x * x + y * y + z * z;
+}
--- utils/Vector3D.h
+++ utils/Vector3D.h
@@ -2,17 +2,31 @@
 // Created by i on 12/17/2021.
 //
 
-#ifndef RAYTRACINGINONEWEEKEND_VECTOR3F_H
-#define RAYTRACINGINONEWEEKEND_VECTOR3F_H
+#ifndef RAYTRACINGINONEWEEKEND_VECTOR3D_H
+#define RAYTRACINGINONEWEEKEND_VECTOR3D_H
 
 
-class Vector3F {
+class Vector3D {
 public:
-    Vector3F();
+    Vector3D();
+    Vector3D(double x, double y, double z);
+
+    Vector3D operator-() const;
+    double operator[](int i) const;
+    double& operator[](int i);
+
+    Vector3D& operator+=(const Vector3D &v);
+    Vector3D& operator*=(double t);
+    Vector3D& operator/=(double t);
+
+    [[nodiscard]] double length() const;
+    [[nodiscard]] double length_squared() const;
 
 public:
-    int x, y, z;
+    double x, y, z;
 };
 
+using Point3D = Vector3D;
+using Color = Vector3D;
 
-#endif //RAYTRACINGINONEWEEKEND_VECTOR3F_H
+#endif //RAYTRACINGINONEWEEKEND_VECTOR3D_H
