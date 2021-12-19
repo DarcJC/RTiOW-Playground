3b9f80abde3410f3977578655e7104e130b2c933
DarcJC <me@darc.pro>
2021-12-20 00:35:58 +0800

    faet(utils): add Ray class

---
+++ utils/Ray.cpp
@@ -0,0 +1,5 @@
+//
+// Created by i on 12/20/2021.
+//
+
+#include "Ray.h"
---
+++ utils/Ray.h
@@ -0,0 +1,14 @@
+//
+// Created by i on 12/20/2021.
+//
+
+#ifndef RAYTRACINGINONEWEEKEND_RAY_H
+#define RAYTRACINGINONEWEEKEND_RAY_H
+
+
+class Ray {
+
+};
+
+
+#endif //RAYTRACINGINONEWEEKEND_RAY_H
