955957a980fb4d3887f93475e6ebb5746d4141e6
DarcJC <me@darc.pro>
2022-01-08 10:45:03 +0800

    feat(ray). add ray class content

--- utils/Ray.cpp
+++ utils/Ray.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "Ray.h"
+
+Ray::Ray() = default;
+
+Ray::Ray(const Point3D &origin, const Vector3D &direction): origin(origin), direction(direction) {}
+
+const Point3D &Ray::getOrigin() const {
+    return origin;
+}
+
+void Ray::setOrigin(const Point3D &origin) {
+    Ray::origin = origin;
+}
+
+const Vector3D &Ray::getDirection() const {
+    return direction;
+}
+
+void Ray::setDirection(const Vector3D &direction) {
+    Ray::direction = direction;
+}
+
+Point3D Ray::at(double t) const {
+    return origin + t * direction;
+};
--- utils/Ray.h
+++ utils/Ray.h
@@ -6,8 +6,27 @@
 #define RAYTRACINGINONEWEEKEND_RAY_H
 
 
+#include "Vector3D.h"
+
 class Ray {
 
+private:
+    Point3D origin;
+    Vector3D direction;
+
+public:
+
+    [[nodiscard]] const Point3D &getOrigin() const;
+    void setOrigin(const Point3D &origin);
+
+    [[nodiscard]] const Vector3D &getDirection() const;
+    void setDirection(const Vector3D &direction);
+
+public:
+    Ray();
+    Ray(const Point3D& origin, const Vector3D& direction);
+
+    [[nodiscard]] Point3D at(double t) const;
 };
 
 
